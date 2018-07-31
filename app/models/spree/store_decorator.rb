2dd1855af659f95a2dcf9decaff5016bc5082f1f
Piotr Biegaj <PeteTheSadPanda@users.noreply.github.com>
2018-07-31 11:31:05 -0500

    Ensure that when a Spree::Store is destroyed, so are its Products

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Store.class_eval do
-  has_and_belongs_to_many :products, join_table: 'spree_products_stores'
+  has_and_belongs_to_many :products, join_table: 'spree_products_stores', dependent: :destroy
   has_many :taxonomies
   has_many :orders
 
