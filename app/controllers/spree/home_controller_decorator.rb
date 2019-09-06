2d0e506e1e80988769b64d7c955ccc697c1bf885
Piotr Biegaj <PeteTheSadPanda@users.noreply.github.com>
2019-09-06 13:33:25 -0500

    more

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,7 +1,11 @@
-Spree::HomeController.class_eval do
-  def index
-    @searcher = build_searcher(params)
-    @products = @searcher.retrieve_products
-    @taxonomies = get_taxonomies
+module Spree
+  module HomeControllerDecorator
+    def index
+      @searcher = build_searcher(params)
+      @products = @searcher.retrieve_products
+      @taxonomies = get_taxonomies
+    end
+
+    Spree::HomeControllerDecorator.prepend(self) if SpreeMultiDomain::Engine.frontend_available?
   end
-end if SpreeMultiDomain::Engine.frontend_available?
+end
