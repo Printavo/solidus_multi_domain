1e54b1babec81588d2711822568a63a5094f54b0
Piotr Biegaj <PeteTheSadPanda@users.noreply.github.com>
2019-09-06 13:48:10 -0500

    more

--- app/controllers/spree/api/products_controller_decorator.rb
+++ app/controllers/spree/api/products_controller_decorator.rb
@@ -1,3 +1,11 @@
-if SpreeMultiDomain::Engine.api_available?
-  Spree::Api::ProductsController.include(SpreeMultiDomain::ShowProductSupport)
+module Spree
+  module Api
+    module ProductsControllerDecorator
+      def prepend(base)
+        base.include(SpreeMultiDomain::ShowProductSupport)
+      end
+
+      Spree::Api::ProductsControllerDecorator.prepend(self) if SpreeMultiDomain::Engine.api_available?
+    end
+  end
 end
