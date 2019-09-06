6cb191f1ebbe58e833a26455a77513fe9f94a624
Piotr Biegaj <PeteTheSadPanda@users.noreply.github.com>
2019-09-06 13:22:42 -0500

    more

--- app/models/spree/taxonomy_decorator.rb
+++ app/models/spree/taxonomy_decorator.rb
@@ -1,3 +1,10 @@
-Spree::Taxonomy.class_eval do
-  belongs_to :store
+module Spree
+  module TaxonomyDecorator
+
+    def prepend(base)
+      base.belongs_to :store
+    end
+
+    Spree::Taxonomy.prepend self
+  end
 end
