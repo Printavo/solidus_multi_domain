af4cef1582a2cab869e477fdb80809682e2e4601
Piotr Biegaj <PeteTheSadPanda@users.noreply.github.com>
2019-09-06 13:45:51 -0500

    more

--- app/controllers/spree/api/shipments_controller_decorator.rb
+++ app/controllers/spree/api/shipments_controller_decorator.rb
@@ -3,13 +3,14 @@ module Spree
     module ShipmentsControllerDecorator
       def mine
         super
+
         @shipments = @shipments.where(spree_orders: { store_id: current_store.id }) if @shipments
+      end
+
+      if SpreeMultiDomain::Engine.api_available?
+        Spree::Api::ShipmentsController.prepend(Spree::Api::ShipmentsControllerDecorator)
+        Spree::Api::ShipmentsController.include(SpreeMultiDomain::CreateLineItemSupport)
       end
     end
   end
-end
-
-if SpreeMultiDomain::Engine.api_available?
-  Spree::Api::ShipmentsController.prepend(Spree::Api::ShipmentsControllerDecorator)
-  Spree::Api::ShipmentsController.include(SpreeMultiDomain::CreateLineItemSupport)
 end
