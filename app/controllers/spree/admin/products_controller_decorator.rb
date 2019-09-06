92926dd125102bcd37676521cc48b60ecca46994
Piotr Biegaj <PeteTheSadPanda@users.noreply.github.com>
2019-09-06 13:31:57 -0500

    more

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,12 +1,18 @@
-Spree::Admin::ProductsController.class_eval do
-  update.before :set_stores
+module Spree
+  module Admin
+    module ProductsControllerDecorator
+      def prepend(base)
+        base.update.before :set_stores
+      end
 
-  private
+      def set_stores
+        # Remove all store associations if store data is being passed and no stores are selected
+        if params[:update_store_ids] && !params[:product].key?(:store_ids)
+          @product.stores.clear
+        end
+      end
 
-  def set_stores
-    # Remove all store associations if store data is being passed and no stores are selected
-    if params[:update_store_ids] && !params[:product].key?(:store_ids)
-      @product.stores.clear
+      Spree::Admin::ProductsController.prepend(self) if SpreeMultiDomain::Engine.admin_available?
     end
   end
-end if SpreeMultiDomain::Engine.admin_available?
+end
