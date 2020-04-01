b5bdda9f5aab9699c60d631c545e7d9fa9623ad3
Piotr Biegaj <PeteTheSadPanda@users.noreply.github.com>
2020-04-01 11:05:32 -0500

    Allow for manual ordering of Merch Products
    TrackerUrl: https://www.pivotaltracker.com/story/show/172068720

--- lib/spree/search/multi_domain.rb
+++ lib/spree/search/multi_domain.rb
@@ -6,12 +6,14 @@ module Spree::Search
       raise MissingStoreIDError unless @properties[:current_store_id].present?
 
       base_scope = Spree::Product.available.by_store(@properties[:current_store_id])
-      base_scope = base_scope.in_taxon(@properties[:taxon]) unless @properties[:taxon].blank?
+      base_scope = base_scope.in_taxon(@properties[:taxon]) if @properties[:taxon].present?
       base_scope = base_scope.where(deactivated: @properties[:deactivated]) unless @properties[:deactivated].nil?
 
-      base_scope = get_products_conditions_for(base_scope, @properties[:keywords]) unless @properties[:keywords].blank?
+      base_scope = get_products_conditions_for(base_scope, @properties[:keywords]) if @properties[:keywords].present?
 
       base_scope = add_search_scopes(base_scope)
+      base_scope = base_scope.order(position: :asc)
+
       base_scope
     end
 
